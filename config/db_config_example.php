b8ffa9b5ed7a9ef50302c76ee387d00e95ce7e22
Bas ten Camp <bas.camp@student.graafschapcollege.nl>
2023-04-06 11:04:42 +0200

    committing the project

---
+++ config/db_config_example.php
@@ -0,0 +1,13 @@
+<?php
+
+$dbhost = 'localhost:3306';
+$dbuser = 'bcamp_root';
+$dbpass = 'bas_gc1305';
+$dbname = 'bcamp_netwerken_2023';
+
+$conn = new mysqli($dbhost,$dbuser,$dbpass,$dbname);
+if($conn->connect_error)
+{
+	die('Failed Connect ! '.$conn->connect_error);
+}
+?>
